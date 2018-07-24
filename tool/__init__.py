2649a0d91de95c5886297ec579293ab3b5284f1c
bs-arno <arno@bsnetworks.net>
2018-07-24 15:18:07 +0800

    初始化

--- tool/__init__.py
+++ tool/__init__.py
@@ -1,5 +1,5 @@
-from tool.client import Client
-from tool.account import Account
+from tool.gd.client import Client
+from tool.gd.account import Account
 
 __version__ = '2.2.5'
 __all__ = ['Client', 'Account', '__version__']
